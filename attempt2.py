3c8012f816d8af449fab896d0a464dbfed903677
shivam-executioner <33172300+shivam-executioner@users.noreply.github.com>
2018-10-21 13:05:10 +0530

    2 cubes

---
+++ attempt2.py
@@ -0,0 +1,152 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+vertices = (
+    (1, -2, -1),
+    (1, 2, -1),
+    (-1, 2, -1),
+    (-1, -2, -1),
+    (1, -2, 1),
+    (1, 2, 1),
+    (-1, -2, 1),
+    (-1, 2, 1)
+    )
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+    )
+
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+    )
+colors = (
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (0,1,0),
+    (1,1,1),
+    (0,1,1),
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (1,0,0),
+    (1,1,1),
+    (0,1,1),
+    )
+#vertices for second cube
+new_vertices = []
+def new_cube():
+    x_change = 3
+    y_change = 0
+    z_change = 0
+
+    # new_vertices = []
+
+    for vert in vertices:
+        new_vert = []
+        x_ch = vert[0] + x_change
+        y_ch = vert[1] + y_change
+        z_ch = vert[2] + z_change
+
+        new_vert.append(x_ch)
+        new_vert.append(y_ch)
+        new_vert.append(z_ch)
+
+        new_vertices.append(new_vert)
+    # return new_vertices
+
+
+def Cube(vertices):
+
+    
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        x = 0
+        for vertex in surface:
+            x+=1
+            glColor3fv(colors[x])
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+
+def main():
+    pygame.init()
+    display = (900,700)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0,0, -10)
+
+    # glRotatef(25, 2, 1, 0)
+
+    #for dictionary cube
+    cube_dict ={}
+    for x in range(2):#for 2 cubes
+        cube_dict[x] = new_cube()
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    glTranslatef(-0.5,0,0)
+                if event.key == pygame.K_RIGHT:
+                    glTranslatef(0.5,0,0)
+
+                if event.key == pygame.K_UP:
+                    glTranslatef(0,1,0)
+                if event.key == pygame.K_DOWN:
+                    glTranslatef(0,-1,0)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    glTranslatef(0,0,1.0)
+
+                if event.button == 5:
+                    glTranslatef(0,0,-1.0)
+
+        #glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        #right cube
+        # for each_cube in cube_dict:
+        #     Cube(cube_dict[each_cube])
+        Cube(new_vertices)
+        #left cube
+        Cube(vertices)
+
+        pygame.display.flip()
+        pygame.time.wait(5)
+
+
+main()
